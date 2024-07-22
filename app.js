00a98073d588603f2432681a9ee81ce85767d6eb
Dimitri V <dvardidze15@gmail.com>
2024-07-22 18:46:44 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    document.getElementById('sharebox').style.display = "block";
+    document.getElementById('popupbtn').style.backgroundColor = "#6c7f99";
+    document.getElementById('share').style.color = "#fff"
+}
+function closePopup() {
+    document.getElementById('sharebox').style.display = "none";
+    document.getElementById('popupbtn').style.backgroundColor = "#ecf1f7";
+}
